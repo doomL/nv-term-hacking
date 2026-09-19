d99681e47f100ad763c2a018f5f368f2e64e247c
Domenico Laurito <laurito.dom@gmail.com>
2026-09-20 01:21:15 +0200

    Restore dud removal footer text (EN/IT), keep dot grid
    Restore dud footer: EN "Dud removed", IT "Parola rimossa"; dots unchanged

--- client/src/utils/gameMessages.ts
+++ client/src/utils/gameMessages.ts
@@ -14,6 +14,8 @@ export function localizeLastMessage(message: string, t: TFunction): string {
       return t('game.accessGranted');
     case 'TERMINAL LOCKED':
       return t('game.terminalLocked');
+    case 'DUD REMOVED':
+      return t('game.dudRemoved');
     case 'ATTEMPTS REPLENISHED':
       return t('game.attemptsReplenished');
     default:
